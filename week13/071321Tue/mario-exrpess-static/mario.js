b9ee3284fb51bf73b26a8f4b5348cccc941e9a25
Greg Hamamgian <hamamgian@yahoo.com>
2021-07-13 21:10:44 -0400

    Week 13 class work

---
+++ week13/071321Tue/mario-exrpess-static/mario.js
@@ -0,0 +1,45 @@
+const http = require('http');
+
+const marioMap = require('./mariomap');
+
+const hostname = '127.0.0.1';
+const port = 3001;
+
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+const helmet = require('helmet');
+app.use(helmet());
+
+const morgan = require('morgan');
+const logger = morgan('tiny');
+app.use(logger);
+
+const es6Renderer = require('express-es6-template-engine');
+app.engine('html', es6Renderer);
+app.set('views', 'templates');
+app.set('view engine', 'html');
+
+const server = http.createServer(app);
+
+app.get('/', (req, res) => {
+    res.render('draw-mario',{
+        locals: {
+            marioMap: marioMap
+        },
+        partials: {
+            myBanner : "/partials/et-banner"
+        }
+    });
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+
+// marioMap.reduce((rows, currRow) => 
+// rows + '<div class="row">' + 
+//     currRow.reduce((cells, currCell) => cells + '<div class="col-auto my-contents" style="background-color:' + currCell + ';"></div>', '')
+// + '</div>', '')
